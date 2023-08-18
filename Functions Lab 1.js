ef64913ba2b175622eee07b952e8255c67404dc6
Francis Ngo <fngo@LABA.PSWAS.LOCAL>
2023-08-18 15:18:41 -0700

    fixed functions by changing them to arrow syntax

--- Functions Lab 1.js
+++ Functions Lab 1.js
@@ -24,21 +24,23 @@ checkName("Jerry");
 checkName("Joyce");
 
 //4. Write a function that called doLoop will do a loop that will count from 0-10. Call this function 3 times.
-function doLoop() {
-    for (let i = 0; i<= 10; i++) {
+const doLoop = () => {
+    for (let i = 0; i <= 10; i++) {
         console.log(i);
     }
-}
+};
 doLoop();
 doLoop();
 doLoop();
 
 //5. Write a function called doLoopWithThisNumber that will do a while loop from 0 until the given number parameter. Call this 5 times with 10, 100, 78, 22, 15
-function doLoopWithThisNumber (number) {
-    for (let i= 0; i < number; i++) {
+const doLoopWithThisNumber = (number) => {
+    let i = 0;
+    while (i < number) {
         console.log(i);
+        i++;
     }
-}
+};
 doLoopWithThisNumber(10);
 doLoopWithThisNumber(100);
 doLoopWithThisNumber(78);
@@ -46,35 +48,37 @@ doLoopWithThisNumber(22);
 doLoopWithThisNumber(15);
 
 //6. Write a function (you make the name) that will add 2 numbers together. Call 3 times.
-function addNumbers(x, y) {
-    return x + y;
-}
+const addNumbers = (x, y) => {
+    x + y;
+};
+
 console.log(addNumbers(3,5));
 console.log(addNumbers(10,7));
 console.log(addNumbers(100,51000));
 
 //7. Write a function (you make the name) that will check if the number parameter is even or odd. call 2 times.
-function checkEvenOdd(number) {
+const checkEvenOdd = (number) => {
     if (number % 2 === 0) {
         console.log("Even");
     } else {
         console.log("Odd");
     }
-}
+};
 checkEvenOdd(10);
 checkEvenOdd(7);
 
 
 //10. Write a function (you make the name) that will log the typeof the data parameter. call 1 time
-function logTypeOf (data) {
+const logTypeOf = (data) => {
     console.log(typeof data);
-}
+};
+logDataType("HelloWord")
 
 //11. Write a function (you make the name) that will do a loop 0-? The loop should end based on the number parameter. call 2 times.
-function doLoopToNumber(number) {
+const doLoopToNumber = (number) => {
     for (let i = 0; i <= number; i++) {
         console.log(i);
     }
-}
+};
 doLoopToNumber(10);
 doLoopToNumber(15);
--- GroupJsLabExtraCredit.js
+++ GroupJsLabExtraCredit.js
@@ -8,7 +8,8 @@ const cat2 = { name: "papaCat", breed: "Egyptian", age: 3 };
 console.log(cat2.breed);
 console.log(cat2.age);
 
-//3 Define combineCats function
+
+// Define combineCats function
 const combineCats = (mama, papa) => {
     console.log("Mama Cat", mama); 
     console.log("Papa Cat", papa); 
@@ -18,5 +19,16 @@ const combineCats = (mama, papa) => {
 combineCats(cat1, cat2);
 console.log(cat1, cat2);
 
-//combineCats({name: "mama", age: 3, breed: "Egyption"} + {name: "papa", age:3, breed:"Calico"} )
-console.log(cat1 + cat2)
+// CombineCats2 function (it won't run if we use combineCats because it is already declared)
+const combineCats2 = (mama, papa) => {
+    const combinedCat2 = {
+        name: mama.name + papa.name,
+        age: 1,
+        breed: mama.breed + "-" + papa.breed
+    };
+    return combinedCat2;
+};
+
+// Call combineCats function with cat1 and cat2 as arguments
+const combinedResult = combineCats2(cat1, cat2);
+console.log(combinedResult);
